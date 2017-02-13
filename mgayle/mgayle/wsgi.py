35096180c6e1e41ed410a221a78d804d56a964e6
al alamin <mdabdullahalalaminp@gmail.com>
2017-02-13 09:56:33 +0600

    little fix

--- mgayle/mgayle/wsgi.py
+++ mgayle/mgayle/wsgi.py
@@ -1,16 +1,33 @@
-"""
-WSGI config for mgayle project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
 
 import os
+import sys
+import site
+
+# Add python site-packages
+site.addsitedir(
+    '/home/alamin/.virtualenvs/pcl/lib/python3.5/site-packages')
+# site.addsitedir('/home/alamin/.virtualenvs/mgayle/lib/python3.5/site-packages')
+
+# your project root directory
+sys.path.append(
+    '/mnt/340048A400486EC4/Dropbox/Programming/Django/mgayle/mgayle')
+
+# directory where your settings.py file is located
+sys.path.append(
+    '/mnt/340048A400486EC4/Dropbox/Programming/Django/mgayle/mgayle/mgayle')
+
+
+
+
+
+# # Activate your virtual env
+# activate_env = os.path.expanduser(
+#     "~/.virtualenvs/eonushilon/bin/activate_this.py")
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "mgayle.settings"
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mgayle.settings")
 
 application = get_wsgi_application()
+
